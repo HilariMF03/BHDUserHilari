484ed1913ac31894741e86624dd690784fb76b76
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-06-06 19:48:29 -0400

    Feat: Registrar servicios por Inyeccion de dependecias.

---
+++ Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+
+namespace Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(option =>
+            {
+                option.EnableSensitiveDataLogging();
+                option.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    m => m.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+            });
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,3 +1,4 @@
+using Persistence;
 
 namespace WebApplication1
 {
@@ -13,6 +14,7 @@ namespace WebApplication1
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddPersistenceInfrastructure(builder.Configuration);
 
             var app = builder.Build();
 
