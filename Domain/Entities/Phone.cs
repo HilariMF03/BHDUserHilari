1e35b7c43b9b180def236770577302f438320bc5
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-06-06 19:21:31 -0400

    Feat: Agregar Entidades.

---
+++ Domain/Entities/Phone.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Domain.Entities
+{
+    public class Phone
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        public string CityCode { get; set; }
+        public string ContryCode { get; set; }
+        public string UserId { get; set; }
+        public Users User { get; set; }
+    }
+}
---
+++ Domain/Entities/Users.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Phone> Phones { get; set; } = new List<Phone>();
+        public string Token { get; set; }
+        public DateTime LastLogin { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Modified { get; set; }
+    }
+}
